d39a19412ecea55a8273f375ea7125a800125b93
Ahmad Waseem <awtabassum@gmail.com>
2024-10-24 17:05:07 +0500

    latest updates 32

--- client/src/screens/Homescreen.jsx
+++ client/src/screens/Homescreen.jsx
@@ -1,14 +1,32 @@
-import React from "react";
-import products from "../products";
+import React, {useEffect,useState} from "react";
+import axios from "axios";
 import Product from "../components/Product";
+
 function Homescreen() {
+  const [products,setProducts]=useState([])
+  useEffect(() => {
+   axios.get('/api/v1/getallproducts').then(res=>{
+    console.log(res);
+    setProducts(res.data)
+  }).catch(err=>{
+    console.log(err);
+    
+  })
+}, [products]) 
+// }, [products])
+  
   return (
     <>
       <h1>Home Screen Page</h1>
-      <div className="row justify-content-center">
-        {products.map((product) => {
-          return <Product product={product} />;
-        })}
+      <div className="row justify-content-center" >
+       {products.length > 0 && (products.map(product=>{
+        return 
+        
+        <div key={product._id}>
+        <Product product = {product}/>
+        </div>
+        
+       }))}
       </div>
     </>
   );
